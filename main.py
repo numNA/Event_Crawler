db885b5a70aaad16d71e9b3aabf71abff6de3113
numNA <47317153+numNA@users.noreply.github.com>
2020-03-27 00:01:56 +0900

    KT rev

--- main.py
+++ main.py
@@ -2,20 +2,12 @@
 
 from coex import get_events as cx_evt
 from kintex import get_events as KT
-#from so import get_jobs as so_job
+
 from save import save_to_file
-# print(ind_job())
 
-# cx_evt()
 KT = KT()
-CX = cx_evt()
-# print(CX)
-EV = KT + CX
-save_to_file(EV)
-print("Complete scrapping and saving the csv file!")
+#CX = cx_evt()
 
-
-#Indeed = ind_job()
-#SO = so_job()
-#jobs = Indeed + SO
-# save_to_file(jobs)
+#EV = KT + CX
+# save_to_file(EV)
+#print("Complete scrapping and saving the csv file!")
